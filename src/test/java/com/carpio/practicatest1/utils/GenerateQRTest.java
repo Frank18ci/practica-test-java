802e20493821077d3db558f99e30d86e4d795fa4
Frank <fcarpio113@gmail.com>
2025-11-05 20:35:29 -0500

    Agregando tests

---
+++ src/test/java/com/carpio/practicatest1/utils/GenerateQRTest.java
@@ -0,0 +1,16 @@
+package com.carpio.practicatest1.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenerateQRTest {
+
+    @Test
+    void generateSvg() {
+    }
+
+    @Test
+    void main() {
+    }
+}
