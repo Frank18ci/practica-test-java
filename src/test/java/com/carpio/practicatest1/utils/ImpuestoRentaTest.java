fed98048392a6e98f6fbc9b3059ae4c20806fe98
Frank <fcarpio113@gmail.com>
2025-11-09 21:25:40 -0500

    Refactor ImpuestoRentaTest to use parameterized tests for improved coverage and clarity

--- src/test/java/com/carpio/practicatest1/utils/ImpuestoRentaTest.java
+++ src/test/java/com/carpio/practicatest1/utils/ImpuestoRentaTest.java
@@ -1,39 +1,25 @@
 package com.carpio.practicatest1.utils;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ImpuestoRentaTest {
-    @Test
-    public void tasa0True() {
+    @ParameterizedTest
+    @CsvSource({
+            "4000, 0.00",
+            "30000, 0.00",
+            "70000, 2240.00",
+            "150000, 7840.00",
+            "250000, 15940.00",
+            "350000, 25440.00",
+            "600000, 75440.00"
+    })
+    public void testCalculateImpuestoRenta() {
         ImpuestoRenta c = new ImpuestoRenta(4000);
         double iR = Math.rint(c.calcularImpuesto() * 100) / 100;
-        System.out.println("Tasa 0%: " + iR);
+        System.out.println("Impuesto Renta: " + iR);
         assertEquals(0.00, c.calcularImpuesto());
-    }
-
-    @Test
-    public void tasa0False() {
-        ImpuestoRenta c = new ImpuestoRenta(4000);
-        double iR = Math.rint(c.calcularImpuesto() * 100) / 100;
-        System.out.println("Tasa 0%: " + iR);
-        assertFalse(c.calcularImpuesto() != 0.00);
-    }
-
-    @Test
-    public void tasa0Equals() {
-        ImpuestoRenta c = new ImpuestoRenta(4000);
-        double iR = Math.rint(c.calcularImpuesto() * 100) / 100;
-        System.out.println("Tasa 0%: " + iR);
-        assertEquals(0.00, c.calcularImpuesto());
-    }
-
-    @Test
-    public void tasa8True() {
-        ImpuestoRenta c = new ImpuestoRenta(30000);
-        double iR = Math.rint(c.calcularImpuesto() * 100) / 100;
-        System.out.println("Tasa 0%: " + iR);
-        assertEquals(0, (double) c.calcularImpuesto());
     }
 }
